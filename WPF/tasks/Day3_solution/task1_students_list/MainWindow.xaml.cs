ea97355b15dec438cc0876508ca0f9a9ba992a2f
a7medfat7y10 <ahemdfathy.1074@gmail.com>
2023-02-07 16:53:50 +0200

    WPF Day3 tasks

---
+++ WPF/tasks/Day3_solution/task1_students_list/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task1_students_list
+{
+    public partial class MainWindow : Window
+    {
+        List<Student> students;
+        public MainWindow()
+        {
+            students = new List<Student>()
+            {
+                new Student(1, "Marwan Fathy", 24, 90, "123 st.", "/1.jpg" ), 
+                new Student(2, "Selim Hashem", 24, 90, "222 st.", "/2.jpg" ),
+                new Student(3, "Morad Selim", 24, 90, "345 st.", "/4.jpg" ),
+                new Student(4, "Salma Omar", 24, 90, "567 st.", "/3.jpg"),
+            };
+            InitializeComponent();
+            lst.ItemsSource = students;
+        }
+    }
+}
---
+++ WPF/tasks/Day3_solution/task1_students_list/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1_students_list
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Grade { get; set; }
+        public string Address { get; set; }
+        public string Image { get; set; }
+        public Student(int _id, string _name, int _age, int _grade, string _address, string _image) {
+            Id = _id;
+            Name = _name;
+            Age = _age;
+            Grade = _grade;
+            Address = _address;
+            Image = _image;
+        }
+        public override string ToString()
+        {
+            return $"{Name}" ;
+        }
+    }
+}
