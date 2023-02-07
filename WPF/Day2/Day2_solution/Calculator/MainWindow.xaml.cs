a6636d6fd76a38cfe986958d3ddb93fae26090e0
a7medfat7y10 <ahemdfathy.1074@gmail.com>
2023-02-07 16:52:53 +0200

    WPF Day 2 tasks

---
+++ WPF/Day2/Day2_solution/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+        double first_num;
+        double result;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //input
+        private void Click_Number(object sender, RoutedEventArgs e)
+        {
+            if (ResultBox.Text == "0")
+                ResultBox.Text = "0";
+            else
+                ResultBox.Text += ((Button)sender).Content.ToString();
+        }
+        private void Dot(object sender, RoutedEventArgs e)
+        {
+            if (!ResultBox.Text.Contains("."))
+                ResultBox.Text += ".";
+        }
+
+        //Operations
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            if (ResultBox.Text == String.Empty)
+                first_num = 0;
+            else
+                first_num = Double.Parse(ResultBox.Text);
+
+            OperationsBox.Text = first_num.ToString() + "+";
+            ResultBox.Text = "";
+        }
+        private void Subtract(object sender, RoutedEventArgs e)
+        {
+            if (ResultBox.Text == String.Empty)
+                first_num = 0;
+            else
+                first_num = Double.Parse(ResultBox.Text);
+
+            OperationsBox.Text = first_num.ToString() + "-";
+            ResultBox.Text = "";
+        }
+        private void Multiply(object sender, RoutedEventArgs e)
+        {
+            if (ResultBox.Text == String.Empty)
+                first_num = 0;
+            else
+                first_num = Double.Parse(ResultBox.Text);
+
+            OperationsBox.Text = first_num.ToString() + "x";
+            ResultBox.Text = "";
+        }
+        private void Division(object sender, RoutedEventArgs e)
+        {
+            if (ResultBox.Text == String.Empty)
+                first_num = 0;
+            else
+                first_num = Double.Parse(ResultBox.Text);
+
+            OperationsBox.Text = first_num.ToString() + "/";
+            ResultBox.Text = "";
+        }
+
+        //Equal
+        private void Click_Equal(object sender, RoutedEventArgs e)
+        {
+            if (ResultBox.Text == String.Empty)
+                result = 0;
+            else if (OperationsBox.Text.Contains("+"))
+                result = first_num + Double.Parse(ResultBox.Text);
+            else if (OperationsBox.Text.Contains("-"))
+                result = first_num - Double.Parse(ResultBox.Text);
+            else if (OperationsBox.Text.Contains("x"))
+                result = first_num * Double.Parse(ResultBox.Text);
+            else if (OperationsBox.Text.Contains("/"))
+                result = first_num / Double.Parse(ResultBox.Text);
+            else
+                result = 0;
+            OperationsBox.Text = string.Empty;
+            ResultBox.Text = result.ToString();
+        }
+
+        //Clear functions
+        private void Clear_one(object sender, RoutedEventArgs e)
+        {
+            if (ResultBox.Text.Length > 0)
+                ResultBox.Text = ResultBox.Text.Substring(0, ResultBox.Text.Length - 1);
+        }
+        private void Clear_All(object sender, RoutedEventArgs e)
+        {
+            ResultBox.Text = String.Empty;
+            OperationsBox.Text = String.Empty;
+        }
+
+    }
+}
---
+++ WPF/Day2/Day2_solution/task2_input_borders/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task2_input_borders
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void border_name(object sender, KeyEventArgs e)
+        {
+            border.BorderBrush = Brushes.Blue;
+        }
+        private void border_address(object sender, KeyEventArgs e)
+        {
+            border2.BorderBrush = Brushes.Blue;
+        }
+    }
+}
