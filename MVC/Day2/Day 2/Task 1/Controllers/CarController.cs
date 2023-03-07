a44f206f4cfa7ff9612ece36999ce76959b2d505
a7medfat7y10 <ahemdfathy.1074@gmail.com>
2023-03-08 00:16:32 +0200

    MVC day 1 and 2 tasks

---
+++ MVC/Day2/Day 2/Task 1/Controllers/CarController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task_1.Models;
+
+namespace Task_1.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET: Car
+        public ActionResult getAll()
+        {
+            List<Car> carLst = CarList.Cars;
+            ViewBag.Cars = carLst;
+            return View();
+        }
+
+        public ActionResult getByID(int id)
+        {
+            Car car = CarList.Cars.FirstOrDefault(e => e.Num == id);
+            ViewBag.selectedCar = car;
+            return View();
+        }
+
+        public ActionResult editByID(int id)
+        {
+            Car car = CarList.Cars.FirstOrDefault(e => e.Num == id);
+            ViewBag.selectedCar = car;
+            return View();
+        }
+
+        public ActionResult EditSave(int Num, string Color, string Model, string Manufacture)
+        {
+            Car editedCar = CarList.Cars.FirstOrDefault(e => e.Num == Num);
+            editedCar.Color = Color; 
+            editedCar.Model = Model;
+            editedCar.Manufacture = Manufacture;
+
+            return RedirectToAction("getAll");
+        }
+
+        public ActionResult createCar()
+        {
+            return View();
+        }
+
+        public ActionResult CreateSave(int Num, string Color, string Model, string Manufacture)
+        {
+            Car car = new Car();
+            car.Num = Num;
+            car.Color = Color;
+            car.Model = Model;
+            car.Manufacture = Manufacture;
+
+            CarList.Cars.Add(car);
+            return RedirectToAction("getAll");
+        }
+
+        public ActionResult deleteByID(int id)
+        {
+            var deletedCar = CarList.Cars.FirstOrDefault(e => e.Num == id);
+            CarList.Cars.Remove(deletedCar);
+            return RedirectToAction("getAll");
+        }
+    }
+}
